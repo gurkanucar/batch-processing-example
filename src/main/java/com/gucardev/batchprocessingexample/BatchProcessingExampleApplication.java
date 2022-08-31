9ce332e382a699e69560b46d260a8be29155e7cc
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-31 03:19:06 +0300

    Initial commit

---
+++ src/main/java/com/gucardev/batchprocessingexample/BatchProcessingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.batchprocessingexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProcessingExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchProcessingExampleApplication.class, args);
+    }
+
+}

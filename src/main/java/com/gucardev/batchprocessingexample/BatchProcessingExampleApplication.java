5c3667cb1379a5991d498351a152aa1626061a97
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-05 00:33:58 +0300

    app configured

--- src/main/java/com/gucardev/batchprocessingexample/BatchProcessingExampleApplication.java
+++ src/main/java/com/gucardev/batchprocessingexample/BatchProcessingExampleApplication.java
@@ -2,8 +2,10 @@ package com.gucardev.batchprocessingexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class BatchProcessingExampleApplication {
 
     public static void main(String[] args) {
